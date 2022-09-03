735cdb7c3879891409735525737b3f1b39d4f0cc
Lord_Evendim <bartezy123@gmail.com>
2022-09-03 23:36:24 +0200

    chore: tenderly

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,7 @@
 import React, { useEffect, useState } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import { Button } from "@chakra-ui/button";
-import { Box, Center, HStack } from "@chakra-ui/layout";
+import { Box, HStack } from "@chakra-ui/layout";
 import { useGlobalState } from "./useGlobal";
 import { AvailableNetworks, networks } from "./networks";
 import { ethers } from "ethers";
@@ -16,7 +15,6 @@ function App() {
   const setProvider = useGlobalState((state) => state.setProvider);
   const setUserAddress = useGlobalState((state) => state.setUserAddress);
   const userAddress = useGlobalState((state) => state.userAddress);
-  const [isConnecting, setIsConnecting] = useState(false);
   const setManager = useGlobalState((state) => state.setManager);
 
   const [addReviewUserId, setAddReviewUserId] = useState("");
@@ -120,66 +118,93 @@ function App() {
   return (
     <div className="App">
       <Box>
-        <Box w={"full"}>
-          <Button onClick={() => handleConnectWallet()}>
+        <Box
+          bg={"blue.300"}
+          w={"full"}
+          display={"flex"}
+          alignItems={"flex-start"}
+        >
+          <Button onClick={() => handleConnectWallet()} m={"20px"}>
             {userAddress ? userAddress : "Connect wallet"}
           </Button>
         </Box>
         <Box h={"200px"} />
-        <VStack gap={"20px"}>
-          <HStack>
-            <Button padding={"30px"} onClick={() => handleAddReview()}>
-              addReview
-            </Button>
-            <Input
-              onChange={(e: any) => setAddReviewUserId(e.target.value)}
-              value={addReviewUserId}
-            ></Input>
-          </HStack>
-          <HStack>
-            <Button padding={"30px"} onClick={() => handleAddLike()}>
-              addLike
-            </Button>
-            <Input
-              onChange={(e: any) => setAddLikeFrom(e.target.value)}
-              value={addLikeFrom}
-            ></Input>
-            <Input
-              onChange={(e: any) => setAddLikeTo(e.target.value)}
-              value={addLikeTo}
-            ></Input>
-          </HStack>
-          <HStack>
-            <Button padding={"30px"} w={"200px"} onClick={() => handleBuy()}>
-              buyMembership
-            </Button>
-            <Input
-              onChange={(e: any) => setBuyId(e.target.value)}
-              value={buyId}
-            ></Input>
-            <Input
-              onChange={(e: any) => setBuyMembership(e.target.value)}
-              value={buyMembership}
-            ></Input>
-          </HStack>
-          <HStack>
-            <Button padding={"30px"} onClick={() => handleWithdraw()}>
-              withdrawTokens
-            </Button>
-            <Input
-              onChange={(e: any) => setWithdrawId(e.target.value)}
-              value={withdrawId}
-            ></Input>
-            <Input
-              onChange={(e: any) => setWithdrawAmount(e.target.value)}
-              value={withdrawAmount}
-            ></Input>
-            <Input
-              onChange={(e: any) => setWithdrawAddress(e.target.value)}
-              value={withdrawAddress}
-            ></Input>
-          </HStack>
-        </VStack>
+        <Box w={"1200px"} mx={"auto"}>
+          <VStack gap={"20px"} w={"full"}>
+            <HStack w={"full"}>
+              <Button
+                padding={"30px"}
+                w={"500px"}
+                onClick={() => handleAddReview()}
+              >
+                addReview
+              </Button>
+              <Input
+                h={"60px"}
+                onChange={(e: any) => setAddReviewUserId(e.target.value)}
+                value={addReviewUserId}
+              ></Input>
+            </HStack>
+            <HStack w={"full"}>
+              <Button
+                padding={"30px"}
+                w={"300px"}
+                onClick={() => handleAddLike()}
+              >
+                addLike
+              </Button>
+              <Input
+                h={"60px"}
+                onChange={(e: any) => setAddLikeFrom(e.target.value)}
+                value={addLikeFrom}
+              ></Input>
+              <Input
+                h={"60px"}
+                onChange={(e: any) => setAddLikeTo(e.target.value)}
+                value={addLikeTo}
+              ></Input>
+            </HStack>
+            <HStack w={"full"}>
+              <Button padding={"30px"} w={"400px"} onClick={() => handleBuy()}>
+                buyMembership
+              </Button>
+              <Input
+                h={"60px"}
+                onChange={(e: any) => setBuyId(e.target.value)}
+                value={buyId}
+              ></Input>
+              <Input
+                h={"60px"}
+                onChange={(e: any) => setBuyMembership(e.target.value)}
+                value={buyMembership}
+              ></Input>
+            </HStack>
+            <HStack>
+              <Button
+                padding={"30px"}
+                w={"500px"}
+                onClick={() => handleWithdraw()}
+              >
+                withdrawTokens
+              </Button>
+              <Input
+                h={"60px"}
+                onChange={(e: any) => setWithdrawId(e.target.value)}
+                value={withdrawId}
+              ></Input>
+              <Input
+                h={"60px"}
+                onChange={(e: any) => setWithdrawAmount(e.target.value)}
+                value={withdrawAmount}
+              ></Input>
+              <Input
+                h={"60px"}
+                onChange={(e: any) => setWithdrawAddress(e.target.value)}
+                value={withdrawAddress}
+              ></Input>
+            </HStack>
+          </VStack>
+        </Box>
       </Box>
     </div>
   );
---
+++ tenderly/actions/hello.js
@@ -0,0 +1,26 @@
+import { BigNumber, ethers, BigNumberish } from "ethers";
+import TokenContract from "../SimpleToken.json";
+
+// Function is later referenced with this name
+const fundsWithdrawalAction = async (context, event) => {
+  // Log so we can later see what's available in payload
+  console.log(event);
+  console.log("Withdrawal has been made");
+  console.log("Fetching the remaining tokens...");
+
+  const TOKEN_ADDRESS = "0x9f8312aFdBfB83C4859cCC4b74CEE74b2ca1ff72";
+  const MANAGER = "0x24378a8CE7d01c586b031ceBfaA5F45de0DFb8CF";
+
+  const contract = new ethers.Contract(TOKEN_ADDRESS, TokenContract.abi);
+  const balanceOf = await contract.balanceOf(MANAGER);
+
+  if (balanceOf.lt(BigNumber.from("100"))) {
+    console.log("WARNING: Low contract balance");
+    return;
+  }
+
+  console.log("INFO: Withdrawal made by a user");
+};
+
+// Function must be exported
+module.exports = { fundsWithdrawalAction };
