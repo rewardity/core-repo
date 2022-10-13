7e55b69c5164048621e2c7376ea374f8591ef945
LordEvendim <bartezy123@gmail.com>
2022-10-13 22:05:07 +0200

    chore(contracts): windows build command and local dev setup

--- contract/hardhat.config.ts
+++ contract/hardhat.config.ts
@@ -12,6 +12,7 @@ const POLYGON_SCAN_API_KEY = process.env.POLYGON_SCAN_API_KEY;
 
 const config: HardhatUserConfig = {
   solidity: "0.8.15",
+  defaultNetwork: "localhost",
   networks: {
     polygonMumbai: {
       url: `https://polygon-mumbai.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
